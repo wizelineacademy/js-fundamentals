1bf81dab1c9bc260173642bcf189ab777344b5ca
Angel Malavar <angel.malavar@gmail.com>
2018-06-09 09:12:18 -0500

    Scripts with missing functionality

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// Notes list that is going to be shown in the browser
+const notes = [{
+  _id: NotesApp.createNoteId(),
+  title: 'Note title goes here!',
+  text: 'This is my very first note text! 😀',
+}, {
+  _id: NotesApp.createNoteId(),
+  title: 'Second awesome note!',
+  text: 'This is my second note text!',
+}];
+
+const addNewNote = NotesApp.addNewNote;
+
+// Named functions
+function getNoteById(_id) {
+  // How can we find a note inside our notes array?
+  // This should return a single note object if found;
+};
+
+const updateNote = function(note) {
+  // Replaces object in array with received one.
+};
+
+// Declared functions
+const insertNote = function(_id, title, text) {
+  // This should receive the title, text and _id of a note as params 
+  // and as its name says, you need to create a new note object and add it to list
+};
+
+const saveNote = () => NotesApp.saveNote(function(_id, title, text) {
+  // Iterate over notes
+  // Validate if note already exists
+  // If note was found, replace values
+  // If not add it
+  // What's do you think missing?
+});
+
+const onClickNote = (event) => NotesApp.onClick(event, function(_id) {
+  console.log('Click button Add Note 🖱');
+  let note;
+  // const note;
+  // You will receive an _id of the note
+  // Construct a note object with the same structure of the list
+  // showNote function will render note in editor
+  NotesApp.showNote(note);
+});
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  console.log('DOM fully loaded and parsed');
+  NotesApp.renderNotesList();
+});
+
+
+
+console.log('You already imported your js file 😎');
---
+++ js/notes-app.js
@@ -0,0 +1,68 @@
+const NotesApp = (() => {
+  // This function only generates an id used for a note;
+  const createNoteId = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const removeNotesList = () => document.getElementById('notesList').innerHTML = '';
+
+  const onClick = (event, cb) => {
+    event.preventDefault();
+    const target = event.target;
+    const note_id = target.getAttribute('id');
+    cb(note_id);
+  };
+
+  // Render li items helper manipulating the DOM
+  const renderNotesList = () => {
+    removeNotesList();
+
+    // Gets <ul></ul> node with given id
+    const notesListNode = document.getElementById('notesList');
+
+    notes.forEach((note) => {
+
+      // Creates <a href></a> node
+      const aNode = document.createElement('a');
+      aNode.classList.add('link');
+      aNode.setAttribute('href', '#');
+      aNode.addEventListener('click', onClickNote);
+
+      // Creates <li></li> node
+      const listNode = document.createElement('li');
+      listNode.classList.add('list-item');
+      listNode.setAttribute('id', note._id);
+      const textnode = document.createTextNode(note.title);
+
+      // Add it inside the its root element
+      listNode.appendChild(textnode);
+      aNode.appendChild(listNode);
+      notesListNode.appendChild(aNode);
+    });
+  };
+
+  const showNote = (note) => {
+    document.getElementById('noteText').value = note.text;
+    document.getElementById('noteTitle').value = note.title;
+    document.getElementById('noteId').value = note._id;
+  };
+
+  const addNewNote = () => {
+    // Clear textarea and title input
+    document.getElementById('noteText').value = '';
+    document.getElementById('noteTitle').value = '';
+    document.getElementById('noteId').value = '';
+  };
+
+  const saveNote = (cb) => {
+    const text = document.getElementById('noteText').value;
+    const _id = document.getElementById('noteId').value || createNoteId();
+    const title = document.getElementById('noteTitle').value;
+    cb(_id, title, text);
+  };
+
+  return {
+    addNewNote,
+    createNoteId,
+    onClick,
+    renderNotesList,
+    showNote,
+  };
+})();
